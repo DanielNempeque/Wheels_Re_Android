ec232d31254d803ca56b296ba647b4396fe49321
DanielNempeque <daniel.nempeque@gmail.com>
2018-11-05 13:16:37 -0500

    Initial commit

---
+++ app/src/main/java/wheels/informatica/unisabana/edu/co/wheels_re/SplashScreen.java
@@ -0,0 +1,16 @@
+package wheels.informatica.unisabana.edu.co.wheels_re;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
